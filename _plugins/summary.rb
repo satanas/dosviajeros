c86509d94fe88163de04808e85278c15bf46b61e
Wil Alvarez <wil.alvarez@groupon.com>
2013-05-28 15:56:06 -0300

    Modified summary plugin

--- _plugins/summary.rb
+++ _plugins/summary.rb
@@ -1,11 +1,13 @@
-module Summary
-  def summary(input)
-    if input.include?('<!-- summary -->')
-      input.split('<!-- summary -->').first
-    else
-      input
+module Jekyll
+  module MyFilters
+    def summary(input)
+      if input.include?('<!-- summary -->')
+        input.split('<!-- summary -->').first
+      else
+        input
+      end
     end
   end
 end
 
-Liquid::Template.register_filter(Summary)
+Liquid::Template.register_filter(Jekyll::MyFilters)
